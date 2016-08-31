d563b9db18b5bcf87fb1a57b0b611a29d0143bde
tomislav <tomislav.bosnjakovic@slobodni-programi.hr>
2016-08-31 12:21:23 +0200

    opz-stat

--- l10n_hr_opz_stat/sql/oe_opz_stat.sql
+++ l10n_hr_opz_stat/sql/oe_opz_stat.sql
@@ -15,7 +15,7 @@ BEGIN
 select *
 from oe_opz_stat(
         _date_to   := '2016-12-31'
-        ,_opz_id   := 38994511
+        ,_opz_id   := 38998192
 )
 
 */
@@ -39,13 +39,13 @@ WITH inv_data AS (
         )
         ,open_move_line AS (
            SELECT aml.partner_id
-                ,inv.date_invoice
-                ,inv.date_due
+                ,coalesce(inv.date_invoice, aml."date") as date_invoice
+                ,coalesce(inv.date_due, aml.date_maturity) as date_due
                 ,inv."id" as invoice_id
-                ,inv."number" as invoice_number
-                ,inv.amount_untaxed as invoice_amount
-                ,inv.amount_tax as invoice_amount_tax
-                ,inv.amount_total as invoice_amount_total
+                ,coalesce(inv."number", aml."name") as invoice_number
+                ,coalesce(inv.amount_untaxed,0.0) as invoice_amount
+                ,coalesce(inv.amount_tax, 0.0) as invoice_amount_tax
+                ,coalesce(inv.amount_total,0.0) as invoice_amount_total
                 ,am.state as move_state
                 ,Coalesce( Nullif(aml.amount_currency, 0.0), aml.debit - aml.credit) as amount_currency
                 --,aml.debit - aml.credit as amount_lcy
@@ -58,7 +58,7 @@ WITH inv_data AS (
                       THEN (Coalesce(Nullif(aml.amount_currency,0.0), aml.debit - aml.credit)) - coalesce(mc.closed_amount, 0.0)
                       ELSE (Coalesce(Nullif(aml.amount_currency,0.0), aml.debit - aml.credit)) + coalesce(mc.closed_amount, 0.0)
                   END as open_amount
-                 ,coalesce(mc.closed_amount, 0.0)  as closed_amount
+                 ,(CASE WHEN coalesce(mc.closed_amount, 0.0) > 0.0 THEN mc.closed_amount ELSE 0.0 END) as closed_amount
                 --,Coalesce(aml.date_maturity, aml."date") as date_maturity
                 --,aml."date" as posting_date
             FROM account_move_line  aml
@@ -67,7 +67,7 @@ WITH inv_data AS (
             --left join account_journal    aj on aj.id=aml.journal_id
             left join res_company        rc on rc.id=aml.company_id
             left join ml_closed          mc on mc.move_line_id=aml.id
-            join account_invoice        inv on inv.move_id = aml.move_id
+            LEFT join account_invoice        inv on inv.move_id = aml.move_id
 
            WHERE 1=1
             /*
@@ -76,9 +76,10 @@ WITH inv_data AS (
                  != 0.0
             */
              AND aa.type in ('receivable')
+             AND (aa.exclude_from_opz_stat is NULL OR aa.exclude_from_opz_stat = False)
              AND coalesce(aml.curr_exch_difference,False)= False
              --AND aml."date" <= _date_from -- (_from_date)
-             AND aml.date_maturity <= _date_to  + INTERVAL '1 month'
+             AND aml.date_maturity <= _date_to  --+ INTERVAL '1 month'
              AND am."state" = 'posted'
              --AND account_account.active
             -- AND (_partner_id = 0 OR coalesce(aml.partner_id, -1) = coalesce(_partner_id, -1) ) --speed
@@ -89,7 +90,7 @@ WITH inv_data AS (
                                ELSE par.vat END), '-') as partner_vat_number
                  ,(CASE WHEN par.vat LIKE 'HR%' THEN 'vat'
                                ELSE 'vatid' END) as partner_vat_type
-                ,oml.date_invoice
+                ,(CASE WHEN oml.date_invoice > oml.date_due THEN oml.date_due ELSE oml.date_invoice END ) AS date_invoice
                 ,oml.date_due
                 ,oml.invoice_id
                 ,oml.invoice_number
@@ -105,7 +106,7 @@ WITH inv_data AS (
                     THEN oml.invoice_amount_total / oml.currency_rate
                     ELSE oml.invoice_amount_total
                 END::numeric),2) as lcy_invoice_amount_total
-                ,('2016-12-31'::date - oml.date_due) as overdue_days
+                ,(_date_to::date - oml.date_due) as overdue_days
                --,oml.move_state::varchar
                --,oml.amount_currency::numeric
                --,oml.amount_lcy::numeric
@@ -123,19 +124,19 @@ WITH inv_data AS (
           FROM open_move_line as oml
           JOIN res_partner par ON par.id = oml.partner_id
           where oml.open_amount > 0.0
-          AND oml.invoice_amount_total > 0.0
+          AND oml.invoice_amount_total >= 0.0
         )
 INSERT INTO opz_stat_line(
        due_date  , partner_name  , invoice_id  , invoice_date  , opz_id , amount_tax          , unpaid          , amount
       ,partner_vat_number  , partner_vat_type , invoice_number  , partner_id  , amount_total          , overdue_days , paid
        ,create_uid, create_date          , write_date           , write_uid)
 SELECT d.date_due, d.partner_name, d.invoice_id, d.date_invoice, _opz_id, d.lcy_invoice_amount_tax, d.open_amount_lcy, d.lcy_invoice_amount
-      ,d.partner_vat_number, d.partner_vat_type, d.invoice_number, d.partner_id, d.lcy_invoice_amount_total, d.overdue_days, d.lcy_invoice_amount_total - d.open_amount_lcy
+      ,d.partner_vat_number, d.partner_vat_type, d.invoice_number, d.partner_id, d.lcy_invoice_amount_total, d.overdue_days, d.closed_amount
        ,1         , timezone('UTC', now()), timezone('UTC', now()), 1
  FROM inv_data d
 WHERE d.open_amount_lcy > 0.0
 AND NOT EXISTS (SELECT 1 FROM opz_stat_line opzl WHERE opzl.partner_id = d.partner_id
-                                                     AND opzl.invoice_id = d.invoice_id
+                                                     AND (opzl.invoice_id IS nOT NULL AND opzl.invoice_id = d.invoice_id)
                                                      AND opzl.opz_id =_opz_id
                     )
 ;
