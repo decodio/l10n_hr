5f34010eed999fe9ac8253fa52ad9994e66f603f
Davor Bojkić <bole@dajmi5.com>
2020-01-13 09:16:08 +0100

    l10n_hr_city - initial commit

---
+++ l10n_hr_city/README.rst
@@ -0,0 +1,34 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+Hrvatska : Gradovi i općine sa poštanskim brojevima
+===================================================
+
+Popis gradova i Županija u RH, sa pripadnim poštanskim brojevima,
+
+Izvor:
+
+Croatian counties (without HR- prefix in code)    https://en.wikipedia.org/wiki/ISO_3166-2:HR
+Hr Pošta webstranice
+
+Bug Tracker
+===========
+
+In case of trouble, please check there if your issue has already been reported.
+If you spotted it first, help us smashing it by providing a detailed and welcomed feedback
+
+Credits
+=======
+
+Contributors
+------------
+
+
+
+Icon
+----
+
+
+
+
---
+++ l10n_hr_city/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    "name": """Croatia - City data""",
+    "summary": """Adds location data for Croatia - Cities, post offices etc.""",
+    "category": "Localisation / Croatia",
+    "images": [],
+    "version": "12.0.1.0.0",
+    "application": False,
+
+    "author": "Decodio",
+    "support": "support@odoo-hrvatska.org",
+    "website": "https://decod.io",
+    "licence": "LGPL-3",
+    #"price" : 20.00,   #-> only if module if sold!
+    #"currency": "EUR",
+
+    "depends": [
+        "base_address_city",
+    ],
+    "external_dependencies": {
+        "python": [],
+        "bin": []
+    },
+    "data": [
+        "data/res_country_state_data.xml",
+        "data/res.city.csv",
+    ],
+    "qweb": [],
+    "demo": [],
+
+    "auto_install": False,
+    "installable": True,
+}
+
