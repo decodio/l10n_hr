a78d6ecb75e8f4bce8c59cc468b0238520b93d16
Davor Bojkić <bole@dajmi5.com>
2020-06-04 15:08:22 +0200

    currency_rate update hnb novi api

---
+++ currency_rate_update_hr_hnb/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2019 Brainbean Apps (https://brainbeanapps.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Currency Rate Update: Croatia-HNB',
+    'version': '12.0.1.0.0',
+    'category': 'Financial Management/Configuration',
+    'summary': 'Update exchange rates using Croatia HNB',
+    'author':
+        'Decodio Applications d.o.o.'
+        'Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'installable': True,
+    'application': False,
+    'depends': [
+        'currency_rate_extended',
+    ],
+    'data': [
+        #'views/res_config_settings.xml',
+        #'views/currency_rate_provider_view.xml',
+    ],
+}
---
+++ currency_rate_update_hr_hnb/models/res_currency_rate_provider_HR_HNB.py
@@ -0,0 +1,104 @@
+# Copyright 2019 Brainbean Apps (https://brainbeanapps.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+import logging
+import json
+from datetime import timedelta
+import urllib.request as request
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+
+_logger = logging.getLogger(__name__)
+
+class ResCurrencyRateProvider_HR_HNB(models.Model):
+    _inherit = 'res.currency.rate.provider'
+
+    service = fields.Selection(
+        selection_add=[('HR-HNB', 'Croatia-HNB')],
+    )
+
+    @api.onchange('service')
+    def onchange_service(self):
+        if not self.service:
+            return {}
+        if self.service != 'HR-HNB':
+            return super(ResCurrencyRateProvider_HR_HNB, self).onchange_service()
+        self.fetch_inverse = True
+        self.provide_rates = 'multi'
+        self.rate_type = 'mid'
+
+    @api.multi
+    def _get_supported_currencies(self):
+        self.ensure_one()
+        if self.service != 'HR-HNB':
+            return super()._get_supported_currencies()  # pragma: no cover
+
+        data = self._l10n_hr_hnb_urlopen()
+        currencies = [c['valuta'] for c in data]
+        # BOLE: ovo paziti jer moguce je da bas na taj dan nemam
+        # sve valute.. ali very low impact pa se ne mucim oko ovog
+        return currencies
+
+    def get_l10n_hr_rate_string(self, rate_type):
+        # TODO: move to l10n_hr nekamo generalnije
+        rates = {
+            'buy': 'kupovni',
+            'mid': 'srednji',
+            'sell': 'prodajni'
+        }
+        return rates.get(rate_type)
+
+    @api.multi
+    def _obtain_rates(self, base_currency, currencies, date_from, date_to):
+        self.ensure_one()
+        if self.service != 'HR-HNB':
+            return super()._obtain_rates(base_currency, currencies, date_from,
+                                         date_to)  # pragma: no cover
+
+        result = {}
+        currencies = [c.name for c in self.currency_ids]
+        data = self._l10n_hr_hnb_urlopen(
+            currencies=currencies,
+            date_from=date_from, date_to=date_to)
+        for cd in data:
+            currency = cd['valuta']
+            if currency not in currencies:
+                continue
+            rate_date = cd['datum_primjene']
+
+            qty = cd['jedinica']
+            rate_string = self.get_l10n_hr_rate_string(self.rate_type)
+            rate = cd.get(rate_string + '_tecaj').replace(',', '.')
+            try:
+                rate = float(rate)
+            except:
+                _logger.debug("HNB problem rate not float: ", str(cd))
+                continue
+
+            # not directly dependant but have in mind
+
+            if not result.get(rate_date):
+                result[rate_date] = {currency: qty / rate}
+            else:
+                result[rate_date].update({currency: qty / rate})
+        return result
+
+
+    @api.multi
+    def _l10n_hr_hnb_urlopen(self, currencies=None, date_from=None, date_to=None):
+        url = "http://api.hnb.hr/tecajn/v2"
+        if date_from is not None:
+            if date_to is not None:
+                url += "?datum-primjene-od=%s&datum-primjene-do=%s" % (
+                    fields.Date.to_string(date_from),
+                    fields.Date.to_string(date_to)
+                )
+        if currencies is not None:
+            for cur in currencies:
+                url += "?valuta=" + cur
+
+        res = request.urlopen(url)
+        data = json.loads(res.read().decode("UTF-8"))
+        return data
+
