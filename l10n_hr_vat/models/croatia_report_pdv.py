7e4e969c13cf932975084db9638ac9eaecf8e631
Davor Bojkić <bole@dajmi5.com>
2020-06-04 15:09:17 +0200

    hr_vat  - mikro dopune

--- l10n_hr_vat/models/croatia_report_pdv.py
+++ l10n_hr_vat/models/croatia_report_pdv.py
@@ -10,7 +10,8 @@ class PdvObrazac(models.Model):
     company_id = fields.Many2one(
         comodel_name="res.company",
         string='Company', required=True,
-        default=lambda self: self.env['res.company']._company_default_get()
+        default=lambda self: self.env['res.company']._company_default_get(),
+        readonly=True, states={'draft': [('readonly', False)]},
     )
     name = fields.Char(string="Name", readonly=1)
     state = fields.Selection(
@@ -25,10 +26,15 @@ class PdvObrazac(models.Model):
             ('7.0', '7.0'),
             ('8.0', '8.0'),
             ('9.0', '9.0'),
-        ], string="Verzija"
+        ], string="Verzija",
+        readonly=True, states={'draft': [('readonly', False)]},
     )
-    date_from = fields.Date()
-    date_to = fields.Date()
+    date_from = fields.Date(
+        readonly=True, states={'draft': [('readonly', False)]},
+    )
+    date_to = fields.Date(
+        readonly=True, states={'draft': [('readonly', False)]},
+    )
 
     # PDV data
     pdv_template_id = fields.Many2one(
@@ -39,6 +45,7 @@ class PdvObrazac(models.Model):
     pdv_line_ids = fields.One2many(
         comodel_name='l10n.hr.pdv.report.pdv',
         inverse_name='report_id',
+        readonly=True, states={'draft': [('readonly', False)]},
         string='PDV')
     pdv_xml_file = fields.Binary('XML File', attachment=True)
     pdv_xml_file_name = fields.Char(string="PDV XML file")
@@ -56,6 +63,13 @@ class PdvObrazac(models.Model):
         string='ZP')
     pdvzp_xml_file = fields.Binary('XML File', attachment=True)
     pdvzp_xml_file_name = fields.Char(string="ZP XML file")
+
+    # PDV-PPO DATA
+    pdvppo_line_ids = fields.One2many(
+        comodel_name='l10n.hr.pdv.report.ppo',
+        inverse_name='report_id',
+        string='PPO'
+    )
 
     def _get_version(self):
         if self.date_to < '2013-07-01':
@@ -81,9 +95,14 @@ class PdvObrazac(models.Model):
                      for tl in self.pdv_template_id.line_ids]
         self.pdv_line_ids = existing + new_lines
 
+
+    def button_get_data(self):
+        return
+
 class PdvObrazacStavka(models.AbstractModel):
     _name = 'l10n.hr.pdv.obrazac.line'
     _description = "Common abstrac report line model"
+
 
     sequence = fields.Integer(
         string="R.br.", required=1
@@ -96,36 +115,47 @@ class PdvObrazacStavka(models.AbstractModel):
         comodel_name='res.partner',
         string="Partner"
     )
-
+    state = fields.Selection(
+        related='report_id.state', readonly=True
+    )
 
 class PdvReportPdv(models.Model):
     _name = 'l10n.hr.pdv.report.pdv'
     _inherit = 'l10n.hr.pdv.obrazac.line'
 
+    state = fields.Selection(
+        related='report_id.state', readonly=True
+    )
     stavka_id = fields.Many2one(
         comodel_name='l10n.hr.config.report.pdv.line',
         string='Definicija stavke',
-        required=True
+        required=True,
+        readonly=True, states={'draft': [('readonly', False)]},
     )
     sequence=fields.Integer(
-        related='stavka_id.sequence'
+        related='stavka_id.sequence',
     )
     code = fields.Char(
-        string="Code",
-        related='stavka_id.code'
+        string="Code", related='stavka_id.code',
+        readonly=True, states={'draft': [('readonly', False)]},
     )
     name = fields.Char(
-        string="Name",
-        related='stavka_id.name'
+        string="Name", related='stavka_id.name',
+        readonly=True, states={'draft': [('readonly', False)]},
     )
-    amount_base = fields.Float()
-    amount_tax = fields.Float()
+    amount_base = fields.Float(
+        readonly=True, states={'draft': [('readonly', False)]},
+    )
+    amount_tax = fields.Float(
+        readonly=True, states={'draft': [('readonly', False)]},
+    )
     move_line_ids = fields.Many2many(
         comodel_name='account.move.line',
         relation='l10n_hr_report_pdv_move_line_rel',
         column1='pdv_line_id', column2='move_line_id',
         string='Stavke temeljnica',
-        help="Stavke temeljnice zbrojene u ovu stavku"
+        help="Stavke temeljnice zbrojene u ovu stavku",
+        readonly=True, states={'draft': [('readonly', False)]},
     )
 
 class PdvReportPDVS(models.Model):
@@ -133,19 +163,28 @@ class PdvReportPDVS(models.Model):
     _inherit = 'l10n.hr.pdv.obrazac.line'
     _description = 'Stavke PDV-S izvjestaja'
 
-
-    partner_id = fields.Many2one(required=1)
+    state = fields.Selection(
+        related='report_id.state', readonly=True
+    )
+    partner_id = fields.Many2one(
+        readonly=True, states={'draft': [('readonly', False)]},
+        required=1)
     partner_vat = fields.Char(
         related='partner_id.vat'
     )
-    dobra = fields.Float()
-    usluge = fields.Float()
+    dobra = fields.Float(
+        readonly=True, states={'draft': [('readonly', False)]},
+    )
+    usluge = fields.Float(
+        readonly=True, states={'draft': [('readonly', False)]},
+    )
     move_line_ids = fields.Many2many(
         comodel_name='account.move.line',
         relation='l10n_hr_report_pdvs_move_line_rel',
         column1='pdvs_line_id', column2='move_line_id',
         string='Stavke temeljnica',
-        help="Stavke temeljnice zbrojene u ovu stavku"
+        help="Stavke temeljnice zbrojene u ovu stavku",
+        readonly=True, states={'draft': [('readonly', False)]},
     )
 
 class PdvReportZP(models.Model):
@@ -153,24 +192,77 @@ class PdvReportZP(models.Model):
     _inherit = 'l10n.hr.pdv.obrazac.line'
     _description = 'Stavke PDV - ZP izvjestaja'
 
-    partner_id = fields.Many2one(required=1)
+    state = fields.Selection(
+        related='report_id.state', readonly=True
+    )
+    partner_id = fields.Many2one(
+        readonly=True, states={'draft': [('readonly', False)]},
+        required=1)
     partner_vat = fields.Char(
         related='partner_id.vat'
     )
     dobra = fields.Float(
         string="Dobra",
-        help="Vrijednost isporuke dobara u HRK")
+        help="Vrijednost isporuke dobara u HRK",
+        readonly=True, states={'draft': [('readonly', False)]},
+    )
     dobra_4263 = fields.Float(
         string="Dobra 42, 63",
+        readonly=True, states={'draft': [('readonly', False)]},
         help="Vrijednost isporuke dobara u postupcima 42 i 63 u HRK")
     dobra_tro = fields.Float(
         string="Dobra - Trostrano",
+        readonly=True, states={'draft': [('readonly', False)]},
         help="Vrijednost isporuke dobara u okviru trostranog posla u HRK")
-    usluge = fields.Float(string="Usluge")
+    usluge = fields.Float(
+        string="Usluge",
+        readonly=True, states={'draft': [('readonly', False)]},
+    )
     move_line_ids = fields.Many2many(
         comodel_name='account.move.line',
         relation='l10n_hr_report_pdvzp_move_line_rel',
         column1='zp_line_id', column2='move_line_id',
         string='Stavke temeljnica',
-        help="Stavke temeljnice zbrojene u ovu stavku"
+        help="Stavke temeljnice zbrojene u ovu stavku",
+        readonly=True, states={'draft': [('readonly', False)]},
     )
+
+class PdvReportPPO(models.Model):
+    _name = 'l10n.hr.pdv.report.ppo'
+    _inherit = 'l10n.hr.pdv.obrazac.line'
+    _description = 'Stavke PDV - PPO izvjestaja'
+
+    # TODO: posebni model, predaje se isključivo tromjesečno!
+    # https://www.rrif.hr/Uputa_o_postupanju_Obrazac_PPO-3063-misljenje.html
+    state = fields.Selection(
+        related='report_id.state', readonly=True
+    )
+    partner_id = fields.Many2one(
+        readonly=True, states={'draft': [('readonly', False)]},
+        required=1)
+    partner_vat = fields.Char(
+        related='partner_id.vat'
+    )
+    amount = fields.Float(
+        readonly=True, states={'draft': [('readonly', False)]},
+    )
+    mjesec_tro = fields.Selection(
+        selection=[
+            (1, 'Prvi mjesec tromjesečja'),
+            (2, 'Drugi mjesec tromjesečja'),
+            (3, 'Treći mjesec tromjesečja')
+        ], string='Mjesec u kojem je obavljena isporuka',
+        required=True,
+        readonly=True, states={'draft': [('readonly', False)]},
+    )
+    move_line_ids = fields.Many2many(
+        comodel_name='account.move.line',
+        relation='l10n_hr_report_ppo_move_line_rel',
+        column1='ppo_line_id', column2='move_line_id',
+        string='Stavke temeljnica',
+        help="Stavke temeljnice zbrojene u ovu stavku",
+        readonly=True, states={'draft': [('readonly', False)]},
+    )
+
+
+
