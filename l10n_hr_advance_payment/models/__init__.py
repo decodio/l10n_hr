8b61ed0f9c3400158995bfaa01150150e2ff1c51
Goran Bogic <goran.bogic@ecodica.eu>
2022-05-26 13:00:30 +0200

    [10011656;Ecodica;IMP,FIX] l10n_hr_advance_payment: added @AccountJournal.advance_payment field, onchange on AccountInvoice.journal_id flags corresponding invoice with that flag(useful for manual invoices and advance payment journal --> invoice consistency) new way of reversing advance payment accounting: done with method @reverse_moves_insert_into_existing. C/p of account module reverse_moves method with extension of parameter @existing_move, tell Odoo on which AccountMove object to insert lines.

--- l10n_hr_advance_payment/models/__init__.py
+++ l10n_hr_advance_payment/models/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from . import account_journal
+from . import account_move
 from . import account_invoice
 from . import sale_order
 from . import purchase_order
--- l10n_hr_advance_payment/models/account_invoice.py
+++ l10n_hr_advance_payment/models/account_invoice.py
@@ -4,27 +4,37 @@ from odoo import fields, models, api
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    advance_payment = fields.Boolean('Advance Payment', index=True, default=False, copy=False)
+    advance_payment = fields.Boolean(string='Advance Payment', index=True, default=False, copy=False)
     advance_invoice_ids = fields.Many2many(
         comodel_name='account.invoice',
         relation='account_advance_invoice_invoice_rel',
         column1='invoice_id', column2='advance_invoice_id',
         string='Advance Invoices', readonly=False, copy=False)
 
+    def _onchange_journal_id(self):
+        res = super()._onchange_journal_id()
+        if self.journal_id.advance_payment:
+            self.advance_payment = self.journal_id.advance_payment
+        return res
+
     def action_move_create(self):
         res = super().action_move_create()
         aml_obj = self.env['account.move.line']
         for invoice in self:
             advance_payment_move_lines = aml_obj
-            for advance in invoice.advance_invoice_ids:
-                invoice_move_line = advance.move_id.line_ids.filtered(
+            for advance in invoice.advance_invoice_ids.filtered(lambda adv: adv.state in {'open', 'paid'}):
+                advance_move_line = advance.move_id.line_ids.filtered(
                     lambda l: l.account_id == advance.account_id and l.full_reconcile_id)
-                advance_payment_move_line = invoice_move_line.full_reconcile_id.reconciled_line_ids - invoice_move_line
+                advance_payment_move_line = advance_move_line.full_reconcile_id.reconciled_line_ids - advance_move_line
+                # remove payment reconciliation
+                advance_payment_move_line.remove_move_reconcile()
                 # save reference to payments for next reconciliation
                 advance_payment_move_lines |= advance_payment_move_line
                 # reverse moves and reconcile it
-                advance.move_id and advance.move_id.reverse_moves(date=invoice.date_invoice)
+                advance.move_id and advance.move_id.reverse_moves_insert_into_existing(date=invoice.date_invoice,
+                                                                                       existing_move=invoice.move_id)
             if advance_payment_move_lines:
-                aml_obj._reconcile_lines(invoice.move_id.line_ids.filtered(lambda l: l.account_id == advance.account_id),
+                aml_obj._reconcile_lines(invoice.move_id.line_ids.
+                                         filtered(lambda l: l.account_id == advance_move_line.account_id),
                                          advance_payment_move_lines, 'amount_residual')
         return res
