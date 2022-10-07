bca62525290fad0f6197e4be3ab5f5a79961bdfc
gogy <goran.bogic@ecodica.eu>
2022-10-07 15:30:29 +0200

    [10011867;Infigo;IMP] l10n_hr_mis_reports: get accounting on analytic tags that are linked to analytic accounts by distribution.

--- l10n_hr_mis_reports/models/__init__.py
+++ l10n_hr_mis_reports/models/__init__.py
@@ -2,3 +2,4 @@
 
 from . import res_company
 from . import mis_report_kpi
+from . import mis_report_instance_period
---
+++ l10n_hr_mis_reports/models/mis_report_instance_period.py
@@ -0,0 +1,23 @@
+from odoo import fields, models, api, tools
+
+
+class MisReportInstancePeriod(models.Model):
+    _inherit = 'mis.report.instance.period'
+
+    @api.model
+    def _get_filter_domain_from_context(self):
+        res = super()._get_filter_domain_from_context()
+        # analytic_tag_filters = list(filter(lambda a: a[0] == 'analytic_tag_ids', res))
+        # if analytic_tag_filters:
+        #     analytic_tags_start_index = res.index(analytic_tag_filters[0])
+        #     res[analytic_tags_start_index:analytic_tags_start_index] = ['|'] * len(analytic_tag_filters)
+        analytic_account_filter = list(filter(lambda a: a[0] == 'analytic_account_id', res))
+        if analytic_account_filter:
+            analytic_account_id = analytic_account_filter[0][2]
+            analytic_tags = self.env['account.analytic.tag'].search(
+                [('analytic_distribution_ids.account_id', '=', analytic_account_id)])
+            if analytic_tags:
+                analytic_account_start_index = res.index(analytic_account_filter[0])
+                res[analytic_account_start_index:analytic_account_start_index] = ['|', (
+                    'analytic_tag_ids', 'in', analytic_tags.ids)]
+        return res
