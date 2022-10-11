3669f75f114a49e1db324da0199ec4882df49f19
gogy <goran.bogic@ecodica.eu>
2022-10-11 20:59:28 +0200

    [10011867;Infigo;FIX] l10n_hr_mis_reports: OR operator counter fix

--- l10n_hr_mis_reports/models/mis_report_instance_period.py
+++ l10n_hr_mis_reports/models/mis_report_instance_period.py
@@ -8,10 +8,13 @@ class MisReportInstancePeriod(models.Model):
     def _get_filter_domain_from_context(self):
         res = super()._get_filter_domain_from_context()
         analytic_tag_filters = list(filter(lambda a: a[0] == 'analytic_tag_ids', res))
-        if analytic_tag_filters:
-            analytic_tags_start_index = res.index(analytic_tag_filters[0])
-            res[analytic_tags_start_index:analytic_tags_start_index] = ['|'] * len(analytic_tag_filters)
         analytic_account_filter = list(filter(lambda a: a[0] == 'analytic_account_id', res))
+        if analytic_tag_filters:
+            or_operator_counter = len(analytic_tag_filters)
+            if not analytic_account_filter:
+                or_operator_counter -= 1
+            analytic_tags_start_index = res.index(analytic_tag_filters[0])
+            res[analytic_tags_start_index:analytic_tags_start_index] = ['|'] * or_operator_counter
         if analytic_account_filter:
             analytic_account_id = analytic_account_filter[0][2]
             analytic_tags = self.env['account.analytic.tag'].search(
