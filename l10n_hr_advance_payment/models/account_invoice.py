b0a908ccedf67ea10b32db2e63d1d0b820ff5296
gogy <goran.bogic@ecodica.eu>
2022-11-07 11:38:34 +0100

    [10011930;Ecodica;IMP] l10n_hr_advance_payment: computed fields for calculating advance invoices totals

--- l10n_hr_advance_payment/models/account_invoice.py
+++ l10n_hr_advance_payment/models/account_invoice.py
@@ -4,18 +4,44 @@ from odoo import fields, models, api
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
+    @api.multi
+    def _calc_total_advance(self):
+        for inv in self.filtered(lambda i: not i.advance_payment):
+            inv.total_adv_amount_untaxed = 0.0
+            inv.total_adv_amount_tax = 0.0
+            inv.total_adv_amount_total = 0.0
+            inv.lcy_total_adv_amount_untaxed = 0.0
+            inv.lcy_total_adv_amount_tax = 0.0
+            inv.lcy_total_adv_amount_total = 0.0
+            for adv_inv in inv.advance_invoice_ids:
+                if adv_inv.state in ('open', 'paid'):
+                    inv.total_adv_amount_untaxed += adv_inv.amount_untaxed
+                    inv.total_adv_amount_tax += adv_inv.amount_tax
+                    inv.total_adv_amount_total += adv_inv.amount_total
+
     advance_payment = fields.Boolean(string='Advance Payment', index=True, default=False, copy=False)
     advance_invoice_ids = fields.Many2many(
         comodel_name='account.invoice',
         relation='account_advance_invoice_invoice_rel',
         column1='invoice_id', column2='advance_invoice_id',
         string='Advance Invoices', readonly=False, copy=False)
+    total_adv_amount_untaxed = fields.Float(
+        string='Total Advance Untaxed Amount', readonly=True, compute=_calc_total_advance)
+    total_adv_amount_tax = fields.Float(
+        string='Total AdvanceTaxes', readonly=True, compute=_calc_total_advance)
+    total_adv_amount_total = fields.Float(
+        string='Total Advance Total', readonly=True, compute=_calc_total_advance)
+    lcy_total_adv_amount_untaxed = fields.Float(
+        string='Total Advance Untaxed Amount LCY', readonly=True, compute=_calc_total_advance)
+    lcy_total_adv_amount_tax = fields.Float(
+        string='Total AdvanceTaxes LCY', readonly=True, compute=_calc_total_advance)
+    lcy_total_adv_amount_total = fields.Float(
+        string='Total Advance Total LCY', readonly=True, compute=_calc_total_advance)
 
     @api.onchange('journal_id')
     def _onchange_journal_id(self):
         res = super()._onchange_journal_id()
-        if self.journal_id.advance_payment:
-            self.advance_payment = self.journal_id.advance_payment
+        self.advance_payment = self.journal_id.advance_payment
         return res
 
     def action_move_create(self):
