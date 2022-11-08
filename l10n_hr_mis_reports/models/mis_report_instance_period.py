a8b215bd758b45dc9eb4ee93881c09d87214b0d3
gogy <goran.bogic@ecodica.eu>
2022-11-08 17:29:30 +0100

    [10011955;Ecodica;IMP] l10n_hr_mis_reports: MIS reports(Instance Period) advanced filtering, OR operators and Analytic account lookup through Analytic Tags and its Analytic distribution

--- l10n_hr_mis_reports/models/mis_report_instance_period.py
+++ l10n_hr_mis_reports/models/mis_report_instance_period.py
@@ -24,3 +24,23 @@ class MisReportInstancePeriod(models.Model):
                 res[analytic_account_start_index:analytic_account_start_index] = ['|', (
                     'analytic_tag_ids', 'in', analytic_tags.ids)]
         return res
+
+    def _get_additional_move_line_filter(self):
+        res = super()._get_additional_move_line_filter()
+        if self.analytic_tag_ids:
+            analytic_tag_filters = list(filter(lambda a: a[0] == 'analytic_tag_ids', res))
+            or_operator_counter = len(analytic_tag_filters) - 1
+            analytic_tags_start_index = res.index(analytic_tag_filters[0])
+            res[analytic_tags_start_index:analytic_tags_start_index] = ['|'] * or_operator_counter
+        if self.analytic_account_id:
+            analytic_account_filter = list(filter(lambda a: a[0] == 'analytic_account_id', res))
+            analytic_account_start_index = res.index(analytic_account_filter[0])
+            analytic_tags = self.env['account.analytic.tag'].search(
+                [('analytic_distribution_ids.account_id', '=', self.analytic_account_id.id)])
+            if analytic_tags:
+                res[analytic_account_start_index:analytic_account_start_index] = \
+                    ['&', '|', ('analytic_tag_ids', 'in', analytic_tags.ids)]
+            else:
+                # add OR operator
+                res[analytic_account_start_index:analytic_account_start_index] = '|'
+        return res
