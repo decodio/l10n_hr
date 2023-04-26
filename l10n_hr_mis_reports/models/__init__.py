b2632a37f0fc97dac77fe053d0bc0ec3e92c6d20
gogy <goran.bogic@ecodica.eu>
2023-04-26 16:12:37 +0200

    [10012406;Infigo;IMP] l10n_hr_mis_reports: adding new MisReportPDReportWizard for generating new PD reports with @ResCompany.pd_report_xlsx_template_file as template for generating. extending MisReportKPI model with new unique reference @report_sheet for purpose of linking Excel template with multiple sheets with KPI lines extending MisReportInstance with new unique identifier @l10n_hr_report_type -> domain on Wizard fields.

--- l10n_hr_mis_reports/models/__init__.py
+++ l10n_hr_mis_reports/models/__init__.py
@@ -2,4 +2,5 @@
 
 from . import res_company
 from . import mis_report_kpi
+from . import mis_report_instance
 from . import mis_report_instance_period
--- l10n_hr_mis_reports/wizard/__init__.py
+++ l10n_hr_mis_reports/wizard/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
 from . import mis_report_gfi_pod_wizard
+from . import mis_report_pd_report_wizard
