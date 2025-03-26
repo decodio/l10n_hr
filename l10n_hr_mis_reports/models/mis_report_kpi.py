fdf164a715d2c58653bf95f69c4c92b61fe2f59b
gogy <goran.bogic@ecodica.eu>
2025-03-26 09:20:07 +0100

    [;Infigo;FIX] l10n_hr_mis_report: obsolete index.

--- l10n_hr_mis_reports/models/mis_report_kpi.py
+++ l10n_hr_mis_reports/models/mis_report_kpi.py
@@ -10,10 +10,7 @@ class MisReportKPI(models.Model):
     @api.model_cr_context
     def _auto_init(self):
         res = super()._auto_init()
-        if not tools.index_exists(self._cr, 'mis_report_kpi_report_position_unique'):
-            self._cr.execute(
-                """CREATE UNIQUE INDEX mis_report_kpi_report_position_unique
-                    ON mis_report_kpi (report_id, report_position) WHERE report_position != 0""")
+        tools.drop_index(self._cr, 'mis_report_kpi_report_position_unique', self._table)
         if not tools.index_exists(self._cr, 'mis_report_kpi_report_sheet_position_unique'):
             self._cr.execute(
                 """CREATE UNIQUE INDEX mis_report_kpi_report_sheet_position_unique
