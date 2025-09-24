e4afa372f4dd0a7e0fcb3ff10d90ae19bc483f1b
gogy <goran.bogic@ecodica.eu>
2025-09-24 11:43:23 +0200

    [2651;Ecodica;FIX] l10n_hr_kpd: minor inconsistencies while testing.

--- l10n_hr_kpd/models/account_invoice_line.py
+++ l10n_hr_kpd/models/account_invoice_line.py
@@ -5,8 +5,8 @@ class AccountInvoiceLine(models.Model):
     _inherit = 'account.invoice.line'
 
     l10n_hr_kpd_id = fields.Many2one(comodel_name='l10n.hr.kpd', string="KPD Code", compute='_compute_l10n_hr_kpd_id',
-                                     store=True, readonly=False, precompute=True,
-                                     domain=[('type', '=', '6')])
+                                     store=True, readonly=False, domain=[('type', '=', '6')])
+
 
     @api.depends('product_id')
     def _compute_l10n_hr_kpd_id(self):
@@ -14,7 +14,7 @@ class AccountInvoiceLine(models.Model):
             kpd = line.product_id.with_context(force_company=line.company_id.id).l10n_hr_property_kpd_id
             if not kpd:
                 kpd = line.product_id.categ_id.l10n_hr_kpd_id
-                if not kpd:
+                if not kpd and line.product_id.categ_id:
                     parent_categories = self.env['product.category'].search(
                         [('id', 'parent_of', line.product_id.categ_id.id),
                          ('l10n_hr_kpd_id', '!=', False)],
