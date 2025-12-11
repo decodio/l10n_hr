4af2ea96dbf67c805785c9305d8467fdb1443bf2
gogy <goran.bogic@ecodica.eu>
2025-12-11 21:45:06 +0100

    [2651;Ecodica;FIX] l10n_hr_vatex_tax_exempt: name_get/name_search

--- l10n_hr_vatex_tax_exempt/models/l10n_hr_vatex_tax_exempt.py
+++ l10n_hr_vatex_tax_exempt/models/l10n_hr_vatex_tax_exempt.py
@@ -1,11 +1,11 @@
 from odoo import models, fields, api
+from odoo.osv import expression
 
 
 class L10nHrVatexTaxExempt(models.Model):
     _name = "l10n.hr.vatex.tax.exempt"
     _description = "Defines VATEX tax category."
     _inherit = ['mail.thread']
-    _rec_name = 'display_name'
 
     code = fields.Char(string="Code", required=True)
     name = fields.Char(string="Name", required=True, translate=True)
@@ -22,3 +22,22 @@ class L10nHrVatexTaxExempt(models.Model):
         for exempt in self:
             if exempt.code and exempt.name:
                 exempt.display_name = exempt.code + ' - ' + exempt.name
+
+    @api.multi
+    def name_get(self):
+        res = []
+        for vatex in self:
+            rec_name = '%s - %s' % (vatex.code, vatex.name)
+            res.append((vatex.id, rec_name))
+        return res
+
+    @api.model
+    def name_search(self, name, args=None, operator="ilike", limit=100):
+        args = args or []
+        domain = []
+        if name:
+            domain = ["|", ("code", operator, name), ("name", operator, name)]
+            if operator in expression.NEGATIVE_TERM_OPERATORS:
+                domain = ["&", "!"] + domain[1:]
+        vatexs = self.search(domain + args, limit=limit)
+        return vatexs.name_get()
